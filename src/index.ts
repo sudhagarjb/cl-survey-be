5925873f3db908af92b46c8104a71553769b9b26
sudhagarjb <sudhagarjb@gmail.com>
2024-02-23 09:24:16 +0530

    updated index app.use

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors())
-app.use(routes);
 
 //validate req
 app.use((_, res, next) => {
@@ -16,6 +15,8 @@ app.use((_, res, next) => {
   res.header('Access-Control-Allow-Headers', '*')
   next()
 })
+
+app.use(routes);
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port);
