8a8399e6c882c619011baf8817b8bc54544c866e
sudhagarjb <sudhagarjb@gmail.com>
2024-02-25 03:14:58 +0530

    Added SurveyRequest table

---
+++ src/models/SurveyRequest.ts
@@ -0,0 +1,38 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'cl_survey_request' })
+export class SurveyRequest {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // @ManyToOne(() => Survey, { nullable: false })
+  @Column({ name: 'survey_id', nullable: false })
+  surveyId: number;
+
+  @Column({ name: 'survey_name' })
+  surveyName: string;
+
+  @Column({ name: 'contact_id', nullable: false })
+  contactId: number;
+
+  @Column({ name: 'contact_email_id', nullable: false })
+  contactEmailId: string;
+
+  @Column({ name: 'survey_url' })
+  surveyUrl: string;
+
+  @Column({ name: 'uuid' })
+  uuid: string;
+
+  @Column({ name: 'meta_data' })
+  metaData: string;
+
+  @Column({ name: 'is_email_sent' })
+  isEmailSent: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
